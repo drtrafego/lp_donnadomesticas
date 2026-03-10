e1946c5e10322055736ff4715831b0f014b747c0
drtrafego <drtrafego@gmail.com>
2026-03-10 10:40:40 -0300

    Add Hotmart Launcher pixel to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         />
                     </noscript>
                 )}
+                {/* Hotmart Launcher */}
+                <script
+                    id="hotmart_launcher_script"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(l,a,u,n,c,h,e,r){l['HotmartLauncherObject']=c;l[c]=l[c]||function(){
+                            (l[c].q=l[c].q||[]).push(arguments)},l[c].l=1*new Date();h=a.createElement(u),
+                            e=a.getElementsByTagName(u)[0];h.async=1;h.src=n;e.parentNode.insertBefore(h,e)
+                            })(window,document,'script','//launcher.hotmart.com/launcher.js','hot');
+
+                            hot('account','02ccc396-e6e1-39d8-86ff-4819f8a8bd27');
+                        `
+                    }}
+                />
             </head>
             <body className={inter.className}>
                 {/* Google Tag Manager (noscript) */}
